0587eb87128d9db81bca5b5f39978bfc46973b60
alicechoi02 <113940298+alicechoi02@users.noreply.github.com>
2022-11-20 18:45:53 -0500

    Add files via upload

--- s3-p1/main.js
+++ s3-p1/main.js
@@ -1,16 +1,27 @@
-var medType = 0; 
+var type = 0; 
 window.addEventListener("load", function() {
-    medType = Math.floor(Math.random() * 11);
+    type = Math.floor(Math.random() * 11);
     makeVideo();
 });
 
 function makeVideo () {
     const iframe = document.getElementById('my-iframe');
-    if (medType <= 4) {
+
+    if (type <= 4) {
+        // calm
         iframe.src = "https://www.youtube.com/embed/YGlIYgd0Mfw";
     } else {
         iframe.src = "https://www.youtube.com/embed/STHwcaQZyUA";
     }
 }
 
+function surveyLink() {
+    if (type <= 4) {
+        // calm
+        window.open('https://forms.gle/x4ri6BxGWuC2s4o38');
+    } else {
+        window.open('https://forms.gle/QejuXYLia6NyQJc69');
+    }
+    
+}
 
