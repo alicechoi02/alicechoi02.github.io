2afb2a96f3ce6a6af86d3ff36aa66e04fb4afeb3
alicechoi02 <113940298+alicechoi02@users.noreply.github.com>
2022-11-20 15:27:43 -0500

    Add files via upload

---
+++ s3-p1/main.js
@@ -0,0 +1,16 @@
+var medType = 0; 
+window.addEventListener("load", function() {
+    medType = Math.floor(Math.random() * 11);
+    makeVideo();
+});
+
+function makeVideo () {
+    const iframe = document.getElementById('my-iframe');
+    if (medType <= 4) {
+        iframe.src = "https://www.youtube.com/embed/YGlIYgd0Mfw";
+    } else {
+        iframe.src = "https://www.youtube.com/embed/naD-2szCB5s";
+    }
+}
+
+
